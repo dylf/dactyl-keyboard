9278a0779cc09226e4ef206382455c3e46c0b473
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2024-04-15 09:24:30 -0400

    Gotta figure out the side wall shift

--- src/dactyl_manuform.py
+++ src/dactyl_manuform.py
@@ -1115,16 +1115,16 @@ def make_dactyl():
                 y_offset = left_wall_lower_y_offset
                 z_offset = left_wall_lower_z_offset
                 # RIDICULOUS HACK 1
-        # elif row == abs(shift_at) and row != 0:
-        #         # if wall_x_offsets[row] > wall_x_offsets[row - 1]:
-        #     y_offset = join_offset_y
-        #     z_offset += join_offset_z
-        #
-        #     return list(pos - np.array([
-        #         wall_x_offsets[row] + join_offset_x,
-        #         -y_offset,
-        #         left_wall_z_offset + z_offset
-        #     ]))
+        elif row == abs(shift_at) and row != 0:
+                # if wall_x_offsets[row] > wall_x_offsets[row - 1]:
+            y_offset = join_offset_y
+            z_offset += join_offset_z
+
+            return list(pos - np.array([
+                wall_x_offsets[row] + join_offset_x,
+                -y_offset,
+                left_wall_z_offset + z_offset
+            ]))
         # elif row < nrows - 1 and wall_x_offsets[row] != wall_x_offsets[row + 1]:
         #     y_offset = +
         #     return list(pos - np.array([
